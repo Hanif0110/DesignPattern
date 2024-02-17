af0fe621c778000498af819e5c75d11b680e5955
Hanif <hanif.nmh.01@gmail.com>
2024-02-17 16:18:32 +0100

    Mise à jour du fichier

--- src/main/java/com/fges/todoapp/dataaccess/TodoDataAccess.java
+++ src/main/java/com/fges/todoapp/dataaccess/TodoDataAccess.java
@@ -4,27 +4,40 @@ import com.fges.todoapp.model.Todo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 // Classe pour accéder aux données des todos
 public class TodoDataAccess {
     private Path filePath;
+    private ObjectMapper objectMapper;
 
     public TodoDataAccess(String fileName) {
         this.filePath = Paths.get(fileName);
+        this.objectMapper = new ObjectMapper();
     }
 
-    public void insertTodo(String todo) throws IOException {
-        // Implémentation de l'insertion d'un todo
-        // Exemple: Ajouter un todo au format JSON
+    public void insertTodo(String todoName) throws IOException {
+        List<Todo> todos = listTodosInternal();
+        todos.add(new Todo(todoName));
+        objectMapper.writeValue(filePath.toFile(), todos);
+        System.out.println("Todo inserted successfully.");
     }
 
     public void listTodos() throws IOException {
-        // Implémentation pour lister tous les todos
-        // Exemple: Lire et afficher les todos depuis un fichier JSON
+        List<Todo> todos = listTodosInternal();
+        todos.forEach(todo -> System.out.println(todo.getName()));
+    }
+
+    private List<Todo> listTodosInternal() throws IOException {
+        if (!Files.exists(filePath)) {
+            return new ArrayList<>();
+        }
+        return List.of(objectMapper.readValue(filePath.toFile(), Todo[].class));
     }
 }
