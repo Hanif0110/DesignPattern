87c562a803bae190b8a8c5acfdad0e5b7cb645b6
Hanif <hanif.nmh.01@gmail.com>
2024-02-17 16:23:14 +0100

    Mise à jour du fichier

--- src/main/java/com/fges/todoapp/model/Todo.java
+++ src/main/java/com/fges/todoapp/model/Todo.java
@@ -1,8 +1,21 @@
 package com.fges.todoapp.model;
 
-// Modèle de données pour un todo
+// Modèle de données pour un todo_
 public class Todo {
     private String name;
 
-    // Constructeurs, getters et setters
+    /* Constructeur qui initialise le nom du todos avec la valeur passée en paramètre*/
+    public Todo(String todoName) {
+        this.name = todoName;
+    }
+
+    // Getter pour obtenir le nom du todo_
+    public String getName() {
+        return name;
+    }
+
+    // Si nécessaire, vous pouvez également ajouter un setter pour le nom
+    public void setName(String name) {
+        this.name = name;
+    }
 }
